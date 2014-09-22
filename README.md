2b0955f4b7daa992d2847012d7cec6e8d64f194d
Thibaud MEUNIER <tmeunier@agglo-lorient.fr>
2014-09-22 20:41:11 +0200

    mviewernoproxy dans le proxy/simple

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ﻿mviewer
 =============
 
-Visualiseur géographique basé sur OpenLayers 3 beta1 et Jquery mobile 1.4.0 inspiré d'un travail réalisé par Metz Métropole.
+Visualiseur géographique basé sur OpenLayers 3 et Jquery mobile 1.4.0 inspiré d'un travail réalisé par Metz Métropole, modifié par GéoBretagne.
--- proxy/simple.php
+++ proxy/simple.php
@@ -1,17 +1,29 @@
 <?
 
-$pos = strrpos($url, "http://DOMAINE/mviewer");
-if ($pos === false) { // note : 3 signes "="
-	$opts = array('http' => array('proxy' => 'tcp://XXX.XXX.XXX.XXX:YYYY', 'request_fulluri' => true));
-	$context = stream_context_create($opts);
-	$url = urldecode($_REQUEST['url']);
-	$s = file_get_contents($url, false, $context);
-} else {
-	// Pas de proxy vers soi-même
-	// On va chercher la ressource sur le disque
-	// Juste un problème : pas valable s'il y a des paramètres ...
-	$url = str_replace('http://DOMAINE/mviewer', '/opt/www/mviewer', $url);
+$domaine='DOMAINE';
+$ip='XXX.XXX.XXX.XXX';
+$port='YYYY';
+$chemin='LOCALPATH';
+
+// ---
+// --- Offrir un moyen de ne passer par un proxy
+// ---
+if (isset($_REQUEST['mviewernoproxy'])&&($_REQUEST['mviewernoproxy']==1))
 	$s = file_get_contents($url);
+else {
+	$pos = strrpos($url, "http://$domaine/");
+	if ($pos === false) { // note : 3 signes "="
+		$opts = array('http' => array('proxy' => "tcp://$ip:$port", 'request_fulluri' => true));
+		$context = stream_context_create($opts);
+		$url = urldecode($_REQUEST['url']);
+		$s = file_get_contents($url, false, $context);
+	} else {
+		// Pas de proxy vers soi-même
+		// On va chercher la ressource sur le disque
+		// Juste un problème : pas valable s'il y a des paramètres ...
+		$url = str_replace("http://$domaine/mviewer", $chemin, $url);
+		$s = file_get_contents($url);
+	}
 }
 
 echo $s;
