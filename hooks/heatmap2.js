1da95af2e9d1a4ae20616342647629aaee45d803
spelhate <seb.pelhate@gmail.com>
2016-03-01 16:33:38 +0100

    Démo accidents mortels - heatmap

--- hooks/heatmap2.js
+++ hooks/heatmap2.js
@@ -2,11 +2,11 @@
     mviewer.hooks.heatmap2 = {};
     mviewer.hooks.heatmap2.layer = new ol.layer.Heatmap({
             source: new ol.source.Vector({
-                url: "demo/iris.geojson",
+                url: "http://geobretagne.fr/geoserver/dreal_b/wfs?request=getFeature&outputFormat=application/json&typename=route_accidents_mortels&srsName=EPSG:4326",
                 format: new ol.format.GeoJSON()
             })            
     });
     mviewer.hooks.heatmap2.handle = function (features) {
-        alert(features[0].properties.TYP_IRIS);
+        alert(features[0].properties.tues_nb + " tué(s) " + features[0].properties.date);
     };  
 } 
