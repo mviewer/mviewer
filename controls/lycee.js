58dc16fe5c9e9b1c14f77eee3f2f4a0fe10829b1
spelhate <seb.pelhate@gmail.com>
2016-05-17 10:34:20 +0200

    Add dynamic sld change in custom control

--- controls/lycee.js
+++ controls/lycee.js
@@ -1,2 +1,15 @@
-mviewer.customControls.lycee = {};
-mviewer.customControls.lycee.msg = function  (str) { alert(str);};
+var idlayer = 'lycee';
+mviewer.customControls[idlayer] = {};
+var layerDefinition = mviewer.getLayer(idlayer);
+console.log(layerDefinition);
+var source = layerDefinition.layer.getSource();
+
+mviewer.customControls[idlayer].setStyle = function  (style) {
+    source.getParams()['STYLES'] = style;
+    layerDefinition.style = style;
+    layerDefinition.fields = ["nom", "libelle"];
+    layerDefinition.aliases = ["Truc", "Muche"];
+    source.changed();
+    var legendUrl = mviewer.setLayerLegend(layerDefinition);
+    $("#legend-" + idlayer).attr("src", legendUrl);    
+};
