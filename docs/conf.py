88a9591a360bed2d743bb0ea3225b745f49722c8
spelhate <seb.pelhate@gmail.com>
2017-06-01 16:16:47 +0200

    Add mviewer logo

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ html_theme_path = ["_themes",]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_images/mviewer_logo_nb.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
