f5d70a6527ad5e4913c4f315cefcc6f916677401
Simon Berthoumieux <35642025+Xueimuohtreb@users.noreply.github.com>
2019-10-14 14:04:29 +0200

    Update mviewer.js

--- js/mviewer.js
+++ js/mviewer.js
@@ -934,9 +934,9 @@ mviewer = (function () {
                     l.setVisible(false);
                     l.set('name', baselayer.label);
                     l.set('blid', baselayer.id);
-		    if (baselayer.opacity !== 'undefined' ) {
-			    l.setOpacity(baselayer.opacity);
-		    }
+		    		if (baselayer.opacity !== 'undefined' ) {
+			    		l.setOpacity(baselayer.opacity);
+		    		}
 
                     _map.addLayer(l);
                     _backgroundLayers.push(l);
@@ -962,9 +962,9 @@ mviewer = (function () {
                             l = new ol.layer.Tile({ source: new ol.source.WMTS(WMTSOptions) });
                             l.set('name', baselayer.label);
                             l.set('blid', baselayer.id);
-		    	    if (baselayer.opacity !== 'undefined' ) {
-			        l.setOpacity(baselayer.opacity);
-		    	    }
+		    	    		if (baselayer.opacity !== 'undefined' ) {
+			        			l.setOpacity(baselayer.opacity);
+		    	    		}
                             _map.getLayers().insertAt(0,l);
                             _backgroundLayers.push(l);
                             if( baselayer.visible === 'true' ) {
@@ -989,11 +989,11 @@ mviewer = (function () {
                 });
                 l.set('name', baselayer.label);
                 l.set('blid', baselayer.id);
-		if (baselayer.opacity !== 'undefined' ) {
-			l.setOpacity(baselayer.opacity);
-		}
+				if (baselayer.opacity !== 'undefined' ) {
+					l.setOpacity(baselayer.opacity);
+				}
                 
-		_backgroundLayers.push(l);
+				_backgroundLayers.push(l);
                 _map.addLayer(l);
                 break;
         }
