3be7bbc50f49a4a6a239853530169a844ed4138a
Pierre Jego <pierre.jego@jdev.fr>
2020-04-13 12:09:04 +0200

    Update VectorContext management
    see https://github.com/openlayers/openlayers/issues/9727 and https://github.com/openlayers/openlayers/blob/master/changelog/upgrade-notes.md#new-getvectorcontext-function-provides-access-to-the-immediate-vector-rendering-api
    
    link to #251

--- js/mviewer.js
+++ js/mviewer.js
@@ -1306,10 +1306,9 @@ mviewer = (function () {
         var listenerKey;
 
         function animate(event) {
-            var vectorContext = event.vectorContext;
-            var frameState = event.frameState;
+            var vectorContext = ol.render.getVectorContext(event);
             var flashGeom = feature.getGeometry().clone();
-            var elapsed = frameState.time - start;
+            var elapsed = event.frameState.time - start;
             var elapsedRatio = elapsed / duration;
             // radius will be 5 at start and 30 at end.
             var radius = ol.easing.easeOut(elapsedRatio) * 25;
@@ -1336,7 +1335,7 @@ mviewer = (function () {
             // tell OL to continue postcompose animation
             _map.render();
         }
-        listenerKey = _map.on('postcompose', animate);
+        listenerKey = _getLayerByName("flash").on('postrender', animate);
     };
 
     var _calculateTicksPositions = function (values) {
