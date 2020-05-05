64ed6ba05dfb96d8c3eb92b246f033bb561feb04
Sfoucheur <sebastien.foucheur@bretagne.bzh>
2020-05-05 12:42:59 +0200

    added highlight on parcelle selected + sort lists

--- demo/cad/control/cad.js
+++ demo/cad/control/cad.js
@@ -1,29 +1,30 @@
-mviewer.customControls.cad = (function() {
+mviewer.customControls.cad = (function () {
     var _data = false;
     var defaultParameters = {
-            "BASEURL": "https://geobretagne.fr/geoserver/cadastre/wfs",
-            "SERVICE": "WFS",
-            "VERSION": "2.0.0",
-            "REQUEST": "GetFeature",
-            "outputFormat": "application/json",
-            "srsName": "EPSG:4326" 
+        "BASEURL": "https://geobretagne.fr/geoserver/cadastre/wfs",
+        "SERVICE": "WFS",
+        "VERSION": "2.0.0",
+        "REQUEST": "GetFeature",
+        "outputFormat": "application/json",
+        "srsName": "EPSG:4326"
     }
-    var querybuilder = function(options){
+    var querybuilder = function (options) {
         options = Object.assign(Object.assign({}, defaultParameters), options);
         let url = "";
         Object.keys(options).forEach(function (key) {
-            if(key==="BASEURL")
-                url = options[key]+"?" + url;
+            if (key === "BASEURL")
+                url = options[key] + "?" + url;
             else
-                url += "&"+key+"="+options[key]
+                url += "&" + key + "=" + options[key]
         });
         return encodeURI(url);
     }
+    
     var setData = function (data) {
         if (!_data) {
             _data = data;
             console.log(data);
-            appendSelect('dep-select', data.departements,"label","value");
+            appendSelect('dep-select', data.departements, "label", "value");
             document.getElementById('dep-select').addEventListener("change", onDptChange);
             document.getElementById('com-select').addEventListener("change", onComChange);
             document.getElementById('section-select').addEventListener("change", onSectionChange);
@@ -31,20 +32,28 @@ mviewer.customControls.cad = (function() {
         }
     };
     var appendSelect = function (select, data, text, value) {
-        let element  = document.getElementById(select);
+        let element = document.getElementById(select);
+        let tempOptions = [];
         emptySelect(select);
-        data.forEach(function(item) {
+        data.forEach(function (item) {
             let option = document.createElement("option");
             option.text = item.properties[text];
             option.value = item.properties[value];
+            tempOptions.push(option);
+        })
+        tempOptions.sort(function(a,b) {
+            return a.innerHTML.localeCompare(b.innerHTML);
+        })
+        tempOptions.forEach(function(option){
             element.add(option);
         })
         
+
     };
-    var emptySelect = function(select){
-        let element  = document.getElementById(select);
+    var emptySelect = function (select) {
+        let element = document.getElementById(select);
         let defaultoption = element.options[0];
-        element.innerHTML="";
+        element.innerHTML = "";
         element.prepend(defaultoption);
         element.selectedIndex = "0";
     }
@@ -61,26 +70,36 @@ mviewer.customControls.cad = (function() {
         mviewer.getMap().getView().fit(src.getExtent());
     };
 
-    var highlightParcelle = function(id_parcelle){
+    var highlightParcelle = function (id_parcelle) {
+        if(previousParcelle){
+            previousParcelle.setStyle(undefined);
+        }
         let layer = mviewer.getLayer('cad').layer;
         let src = layer.getSource();
-        src.forEachFeature(function(feature){
-            console.log("ID : ",id_parcelle,"feature : ",feature);
+        let f = false;
+        src.forEachFeature(function (feature) {
+            if (feature.get("geo_parcelle") === id_parcelle) {
+                feature.setStyle(mviewer.customLayers.cad.highlightStyle);
+                f=feature;
+                return true;
+            }
+            return false;
         });
+        return f;
     }
     var onDptChange = function (e) {
         let selectedDpt = e.target.value;
         let options = {
             "TYPENAME": "CP.CadastralZoningLevel1",
             "PROPERTYNAME": "nom_commune,geo_commune",
-            "CQL_FILTER": "departement='"+selectedDpt+"'"
+            "CQL_FILTER": "departement='" + selectedDpt + "'"
         }
         document.getElementById('loading-cad').style.display = "block";
         fetch(querybuilder(options)).then(
             response => {
                 response.json().then(
                     data => {
-                        appendSelect("com-select",data.features,"nom_commune","geo_commune");
+                        appendSelect("com-select", data.features, "nom_commune", "geo_commune");
                         emptySelect("section-select");
                         emptySelect("parcelle-select");
                     }
@@ -93,79 +112,65 @@ mviewer.customControls.cad = (function() {
         let selectedCom = e.target.value;
         let options = {
             "TYPENAME": "CP.CadastralZoning",
-            "CQL_FILTER": "geo_commune='"+selectedCom+"'"
-            
+            "CQL_FILTER": "geo_commune='" + selectedCom + "'"
+
         }
         document.getElementById('loading-cad').style.display = "block";
         fetch(querybuilder(options))
-        .then(
-            response => {
-                response.json().then(
-                    data => {
-                        appendSelect("section-select",data.features,"label","geo_section");
-                        emptySelect("parcelle-select");
-                    }
-                ).then(() => document.getElementById('loading-cad').style.display = "none")
-            }
-        );
+            .then(
+                response => {
+                    response.json().then(
+                        data => {
+                            appendSelect("section-select", data.features, "label", "geo_section");
+                            emptySelect("parcelle-select");
+                        }
+                    ).then(() => document.getElementById('loading-cad').style.display = "none")
+                }
+            );
     };
     var onSectionChange = function (e) {
         let selectedSection = e.target.value;
         let options = {
             "TYPENAME": "CP.CadastralParcel",
-            "CQL_FILTER": "geo_section='"+selectedSection+"'"
-            
+            "CQL_FILTER": "geo_section='" + selectedSection + "'"
+
         }
         document.getElementById('loading-cad').style.display = "block";
         fetch(querybuilder(options))
-        .then(
-            response => {
-                response.json().then(
-                    data => {
-                        setLayerSource(data);
-                        appendSelect("parcelle-select",data.features,"label","geo_parcelle");
-                    }
-                ).then(() => document.getElementById('loading-cad').style.display = "none")
-            }
-        );
+            .then(
+                response => {
+                    response.json().then(
+                        data => {
+                            setLayerSource(data);
+                            appendSelect("parcelle-select", data.features, "label", "geo_parcelle");
+                        }
+                    ).then(() => document.getElementById('loading-cad').style.display = "none")
+                }
+            );
     };
-    var onParcelleChange = function(e){
+    var previousParcelle = false;
+    var onParcelleChange = function (e) {
         let selectedparcelle = e.target.value;
-        let options = {
-            "TYPENAME": "CP.CadastralParcel",
-            "CQL_FILTER": "geo_section='"+selectedSection+"'"
-            
-        }
-        
-        fetch(querybuilder(options))
-        .then(
-            response => {
-                response.json().then(
-                    data => {
-                        highlightParcelle(selectedparcelle);
-                    }
-                )
-            }
-        );
+        previousParcelle = highlightParcelle(selectedparcelle);
     }
     return {
         /*
          * Public
          */
 
-        init: function() {
+        init: function () {
             if (!_data) {
                 fetch('demo/cad/data/data.json')
-                .then(function (response) {
-                    response.json()
-                        .then(function (data) {
-                            setData(data);
-                        });
-                });
+                    .then(function (response) {
+                        response.json()
+                            .then(function (data) {
+                                setData(data);
+                            });
+                    });
             }
         },
 
-        destroy: function() {
+        destroy: function () {
             console.log('destroy');
         }
     };
--- demo/cad/layer.js
+++ demo/cad/layer.js
@@ -2,10 +2,22 @@ mviewer.customLayers.cad = (function () {
     var _layer = new ol.layer.Vector({
         source: new ol.source.Vector()
     });
+    var _highlightStyle = new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,1)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#ff0000',
+            width: 4,
+            opacity: 1
+        }),
+        zIndex:1
+    });
     var _handle = function(){
         
     }
     return {
-        layer: _layer
+        layer: _layer,
+        highlightStyle: _highlightStyle
     };
 }());
