4001c3290bca92147898e92e0141da24e0812201
spelhate <sebastien.pelhate@bretagne.bzh>
2020-05-27 11:20:15 +0200

    Delete folders to move

--- customlayers/isochrones.js
+++
@@ -1,8 +0,0 @@
-{
-mviewer.customLayers.isochrones = {};
-
-mviewer.customLayers.isochrones.layer = new ol.layer.Vector({
-    source: new ol.source.Vector()    
-});
-mviewer.customLayers.isochrones.handle = false;
-}
