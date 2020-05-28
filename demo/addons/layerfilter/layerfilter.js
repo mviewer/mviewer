0891b6abeae53432f6bbce108bdb4fe2565ba5a5
Tobias Kohr <tobias.kohr@camptocamp.com>
2020-05-28 14:28:18 +0200

    [layerfilter] improves results in layertree by limiting filtered text to one node

--- demo/addons/layerfilter/layerfilter.js
+++ demo/addons/layerfilter/layerfilter.js
@@ -4,18 +4,22 @@ const layerfilter = (function() {
   let _layerfilter = function (e) {
       const term = $("#layerfilter-field").val().toLowerCase().trim();
       $("#menu li").hide().filter(function() {
-        const occurs = $(this).text().toLowerCase().trim().indexOf(term) !== -1;
-        if (occurs && term !== "") {
-          // open and display themes and groupes
-          if ($(this).parent("#menu") || $(this).parent().parent("#menu")) {
-            $(this).addClass("opened");
-            $(this).show();
-            $(this).children("ul").show();
-            $(this).children().children("li").show();
-          }
-        } else {
+        // display decision for currently filtered theme, group or layer 
+        const occurs = $(this).children("a").text().toLowerCase().trim().indexOf(term) !== -1;
+        if (term === ""){
           $(this).removeClass("opened");
           $(this).children("ul").hide();
+        } else if (occurs) {
+          // display and open themes and groups if filtered layer matches
+          $(this).parents().show();
+          $(this).parents("li").addClass("opened");
+          // display and open groups and layers if filtered theme or group matches
+          if ($(this).parent("#menu").length === 1 || $(this).parent().parent().parent("#menu").length === 1) {
+            $(this).children("ul").show();
+            $(this).children().children("li").show();
+            $(this).children().children().children("ul").show();
+            $(this).children().children().children().children("li").show();
+          }
         }
         return occurs;
       }).show();
