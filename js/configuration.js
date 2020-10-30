ae42334431fd65dbfce9fcbcd9bc21c2cae96eae
spelhate <sebastien.pelhate@bretagne.bzh>
2020-10-30 14:36:06 +0100

    tag develop branch as 3.4

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.4-snapshot";
+    var VERSION = "3.4";
 
     var _showhelp_startup = false;
 
