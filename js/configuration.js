9ca353eefbc2e97bea84ef97af7bf2eb924d472e
spelhate <sebastien.pelhate@bretagne.bzh>
2021-05-28 09:05:53 +0200

    tag current release - 3.5.2

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.6-snapshot";
+    var VERSION = "3.5.2";
 
     var _showhelp_startup = false;
 
