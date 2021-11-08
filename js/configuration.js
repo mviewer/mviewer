05b01bb4452175998f1381ed9f88054e6a3614af
gaetanbrl <gaetan.bruel@jdev.fr>
2021-11-08 14:38:23 +0100

    Target new release 3.6

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.6-snapshot";
+    var VERSION = "3.6";
 
     var _showhelp_startup = false;
 
