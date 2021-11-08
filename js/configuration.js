4ed048f872cab9bd6abc67c984609fc523deb0fc
gaetanbrl <gaetan.bruel@jdev.fr>
2021-11-08 16:02:35 +0100

    upg version note

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.6";
+    var VERSION = "3.6.1-snapshot";
 
     var _showhelp_startup = false;
 
