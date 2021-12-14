79b56b6b03bfacd3946ae54801f1152663a2a7b5
Loïc E <loic.ecault@bretagne.bzh>
2021-12-14 15:31:10 +0100

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.6.1";
+    var VERSION = "3.7-snapshot";
 
     var _showhelp_startup = false;
 
