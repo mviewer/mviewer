55997fc5b3b3c4e7b338569a7c5fe1d6dd34a0a2
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2022-04-22 09:48:47 +0000

    Demo linked to #503

---
+++ demo/wmswfs/wmswfs.js
@@ -0,0 +1,27 @@
+const layerid = "wmswfs";
+const cc = (function() {
+
+    /*
+    * Private
+    */
+
+    var _layer;
+
+    return {
+        /*
+        * Public
+        */
+
+        init: function () {
+            // mandatory - code executed when layer is added to legend panel
+            mviewer.getLayer("geofla_commune_2015").layer.setVisible(true);
+        },
+
+        destroy: function () {
+            // mandatory - code executed when layer panel is closed
+            mviewer.getLayer("geofla_commune_2015").layer.setVisible(false);
+        }
+    };
+
+}());
+new CustomControl(layerid, cc.init, cc.destroy);
