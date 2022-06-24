cf9f693d0f6e61924689a4cccaa5b51fce8f5fca
Loïc E <loic.ecault@bretagne.bzh>
2022-06-24 16:41:41 +0200

    3.8.1-snapshot

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.8";
+    var VERSION = "3.8.1-snapshot";
 
     var _showhelp_startup = false;
 
