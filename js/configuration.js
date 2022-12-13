366d83578ee1295f5be3ded1523ae95379751884
gaetanbrl <gaetan.bruel@jdev.fr>
2022-12-13 16:39:29 +0100

    release 3.9.1

--- js/configuration.js
+++ js/configuration.js
@@ -9,7 +9,7 @@ var configuration = (function () {
 
     // Mviewer version a saisir manuellement
 
-    var VERSION = "3.10-snapshot";
+    var VERSION = "3.9.1";
 
     var _showhelp_startup = false;
 
