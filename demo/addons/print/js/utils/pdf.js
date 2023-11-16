1ab3402f73090d7f2b79555529dbaf921059fadd
gaetanbrl <gaetan.bruel@jdev.fr>
2023-11-16 16:02:27 +0100

    Fix print and delete block

--- demo/addons/print/js/utils/pdf.js
+++ demo/addons/print/js/utils/pdf.js
@@ -12,7 +12,7 @@ const defaultOptions = {
   // See official doc for more type and quality options :
   // https://ekoopmans.github.io/html2pdf.js/#image-type-and-quality
   image: { type: "jpeg", quality: 1 },
-  html2canvas: { scale: 4, useCORS: true },
+  html2canvas: { scale: 1, useCORS: true },
   // format could be string as "A4"
   // or an array with custom values [595.28, 841.89] in unit specified
   jsPDF: { unit: "mm", format: "A4", orientation: "landscape" },
