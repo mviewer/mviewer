56e22294d374b17d4cdb3b255ee49e3430aa824c
Agath.Adam <agathe.adam@jdev.fr>
2024-03-05 16:23:35 +0100

    Change version

--- js/configuration.js
+++ js/configuration.js
@@ -8,7 +8,7 @@ var configuration = (function () {
 
   // Mviewer version a saisir manuellement
 
-  var VERSION = "3.10.1";
+  var VERSION = "3.10.2-snapshot";
 
   var _showhelp_startup = false;
 
