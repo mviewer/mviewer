7e318b96e78f7930bda91df1550ac2d071143f01
raphael-dsc <raphael.desousacastro@gmail.com>
2024-04-16 09:26:08 +0200

    Mise en commentaire du style dans le customlayer

--- demo/trackview/customlayers/parcours.js
+++ demo/trackview/customlayers/parcours.js
@@ -1,7 +1,8 @@
 {
     mviewer.customLayers.parcours_1 = {};
     var parcours = mviewer.customLayers.parcours_1;
-  
+
+    /*
     parcours.legend = {
       items: [
         {
@@ -15,14 +16,13 @@
         },
       ],
     };
+    */
   
     parcours.layer = new ol.layer.Vector({
       source: new ol.source.Vector({
         url: "demo/trackview/data/swimrun_lineaire.geojson", // On spécifie le fichier de données qu'on souhaite utiliser
         format: new ol.format.GeoJSON(),
       }),
-      // C'est ici que l'on va gérer le style
-      style: parcours.legend.items[0].styles,
     });
   
     parcours.handle = false;
