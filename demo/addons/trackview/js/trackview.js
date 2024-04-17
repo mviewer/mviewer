6bb0661104d3ca250595b064cb215e2f6b319a1d
raphael-dsc <raphael.desousacastro@gmail.com>
2024-04-17 11:33:38 +0200

    Configuration pour #6

--- demo/addons/trackview/js/trackview.js
+++ demo/addons/trackview/js/trackview.js
@@ -16,72 +16,80 @@ var trackview = (function () {
     urlData: global.data.url
   };
   
+  // Permet de définir la légende
+  const style = {
+    'Point': new ol.style.Style({
+      image: new ol.style.Circle({
+        fill: new ol.style.Fill({
+          color: "#0000FF",
+        }),
+        radius: 5,
+        stroke: new ol.style.Stroke({
+          color: "#00FF00",
+          width: 1,
+        }),
+      }),
+    }),
+    'LineString': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: "#FF0000",
+        width: 3,
+      }),
+    }),
+    'MultiLineString': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: "#FFFF00",
+        width: 3,
+      }),
+    }),
+  };
+
+  const vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: parcoursLayer.urlData,
+      format: new ol.format.GPX(),
+    }),
+    style: function(feature) {
+      return style[feature.getGeometry().getType()];
+    },
+  });
+  
   parcoursLayer.legend = {
     items: [
       {
-        label: global.title,
-        geometry: global.style.geometry,
+        label: "Circuit",
+        geometry: "LineString",
         styles: [
-          new ol.style.Style({
-            stroke: new ol.style.Stroke({ color: global.style.color, width: global.style.width }),
-          }),
+          style["LineString"],
+        ],
+      },
+      {
+        label: "Point",
+        geometry: "Point",
+        styles: [
+          style["Point"],
         ],
       },
     ],
   };
   
-  // Creation du layer openLayer
-  var l = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      url: parcoursLayer.urlData,
-      format: new ol.format.readProjection(url),
-    }),
-  });
-
-  
-  mviewer.processLayer(parcoursLayer, l);
+  mviewer.processLayer(parcoursLayer, vector);
   //l.setVisible(true); // Pas assez optimisé => la ligne du dessous fait la même chose mais avec des options supplémentaires (legend)
   mviewer.addLayer(parcoursLayer);
 
-  // Variables globales
-  /*
-  
-  var layerID = mviewer.customComponents.trackview.config.options.mviewer.parcours.stats[0].layerId;
-  console.log(layerID);  // Affichage dans les logs
-
-  var mvLayer = mviewer.getLayer(layerID).layer;
-  console.log(mvLayer);  // Affichage dans les logs
-  */
 
   var _initTool= function () {
     console.log("Initialisation de l'outil"); // Affichage dans les logs
 
     mviewer.getMap().once("rendercomplete", function (e) {
-      var source = l.getSource();
+      var source = vector.getSource();
       console.log(source); // Affichage dans les logs
       var feature = source.getFeatures();
       console.log(feature); // Affichage dans les logs
-      //console.log(feature[0].getGeometry().getExtent()); // Affichage dans les logs
-      //console.log(feature[0].getGeometry().getExtent()[0]); // Affichage dans les logs
-      //mviewer.zoomToLocation(feature[0].getGeometry().getExtent()[0], feature[0].getGeometry().getExtent()[1], 12, null, "EPSG:3857");
       mviewer.getMap().getView().fit(feature[0].getGeometry().getExtent(), {
         duration: 4000, // Permet de définir le temps de l'animation en ms
       });
-    _setStyle();
     })
-  };
-
-  var _setStyle= function () {
-    console.log("Initialisation du style");
-
-    let style = global.style; // On récupère le tableau contenant les différents styles
-    console.log(style);
-
-    l.setStyle({
-      "geometry": style.geometry,
-      "stroke-color": style.color,
-      "stroke-width": style.width,
-    });
   };
 
 return {
