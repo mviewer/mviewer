fc2b3c740685dc753e732333b7cf363d8f37a554
raphael-dsc <raphael.desousacastro@gmail.com>
2024-04-19 11:19:37 +0200

    Modif

--- demo/addons/trackview/js/trackview.js
+++ demo/addons/trackview/js/trackview.js
@@ -26,6 +26,7 @@ var trackview = (function () {
     }),
   };
 
+  // Permet de réutiliser les features plus tard
   const vectorSource = new ol.source.Vector({
     url: parcoursLayer.urlData,
     format: new ol.format.GPX(),
@@ -36,24 +37,6 @@ var trackview = (function () {
     style: function(feature) {
       return style["styleCircuit"];
     },
-  });
-
-  vectorSource.on("featuresloadend", function (a) {
-    let features = vectorSource.getFeatures();
-    let coordinates = features[0].getGeometry().getCoordinates()[0];
-      
-    for (let i = 0; i < coordinates.length; i++) {
-      let featureId = i ;
-      let featureX = coordinates[i][0] ;
-      let featureY = coordinates[i][1] ;
-      let featureZ = coordinates[i][2] ;
-
-      let featureTab = [];
-
-      featureTab.push("Id :",featureId, "x :",featureX, "y :",featureY, "z :",featureZ);
-
-      console.log(featureTab);
-    }
   });
 
   parcoursLayer.legend = {
@@ -67,10 +50,45 @@ var trackview = (function () {
       },
     ],
   };
+
+  var _addGraph = function () {
+    let features = vectorSource.getFeatures();
+    let coordinates = features[0].getGeometry().getCoordinates()[0];
+    let featureTab = [];
+  
+
+    for (let i = 0; i < coordinates.length; i++) {
+      let featureId = i ;
+      let featureX = coordinates[i][0] ;
+      let featureY = coordinates[i][1] ;
+      let featureZ = coordinates[i][2] ;
+      let featureT = coordinates[i][3] ;
+
+      featureTab.push("Id :",featureId, "x :",featureX, "y :",featureY, "z :",featureZ, "Time :",featureT);
+    };
+    new Chart(document.getElementById("trackview-panel"), {
+      type: 'line',
+      data: {
+        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        datasets: [{ 
+            data: [86,114,106,106,107,111,133,221,783,2478],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'World population per region (in millions)'
+        }
+      }
+    });
+  };
   
   mviewer.processLayer(parcoursLayer, vector);
-  //l.setVisible(true); // Pas assez optimisé => la ligne du dessous fait la même chose mais avec des options supplémentaires (legend)
-  mviewer.addLayer(parcoursLayer);
+  mviewer.addLayer(parcoursLayer); // setVisible(true) => n'ajoute pas la légende
 
 
   var _initTool= function () {
@@ -82,9 +100,10 @@ var trackview = (function () {
       var feature = source.getFeatures();
       console.log(feature); // Affichage dans les logs
       mviewer.getMap().getView().fit(feature[0].getGeometry().getExtent(), {
-        duration: 4000, // Permet de définir le temps de l'animation en ms
+        duration: 3000, // Permet de définir le temps de l'animation en ms
       });
-    })
+      _addGraph();
+    });
   };
 
 return {
