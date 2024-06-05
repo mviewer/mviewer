f0a1c09c311858e2256ad032f2fbff659e88bd71
raphael-dsc <raphael.desousacastro@gmail.com>
2024-06-05 13:57:09 +0200

    New dropdown

---
+++ demo/addons/trackview/js/custom-dropdown.js
@@ -0,0 +1,34 @@
+export var initDropdown = () => {
+    const dropdowns = document.querySelectorAll(".dropdown");
+
+    dropdowns.forEach(dropdown => {
+        const select = dropdown.querySelector(".select");
+        const caret = dropdown.querySelector(".caret");
+        const menu = dropdown.querySelector(".menu");
+        const options = dropdown.querySelector(".menu li");
+        const selected = dropdown.querySelector(".selected");
+
+        select.addEventListener("click", () => {
+            select.classList.toggle("select-clicked");
+
+            caret.classList.toggle("caret-rotate");
+
+            menu.classList.toggle("menu-open");
+        });
+
+        options.forEach(option => {
+            option.addEventListener("click", () => {
+                selected.innerText = option.innerText;
+                select.classList.remove("selected-clicked");
+                caret.classList.remove("caret-rotate");
+                menu.classList.remove("menu-open");
+        
+                options.forEach(option => {
+                    option.classList.remove("active");
+                });
+        
+                option.classList.add("active");
+            }); 
+        });
+    });
+};
--- demo/addons/trackview/js/trackview.js
+++ demo/addons/trackview/js/trackview.js
@@ -1,3 +1,5 @@
+import { initDropdown } from "./custom-dropdown";
+
 var trackview = (function () {
 
   var listeParcours = null;
@@ -479,6 +481,41 @@ var trackview = (function () {
     });
   }
   
+  document.addEventListener("DOMContentLoaded", () => {
+    const dropdowns = document.querySelectorAll(".dropdown");
+
+    dropdowns.forEach(dropdown => {
+      const select = dropdown.querySelector(".select");
+      const caret = dropdown.querySelector(".caret");
+      const menu = dropdown.querySelector(".menu");
+      const options = dropdown.querySelector(".menu li");
+      const selected = dropdown.querySelector(".selected");
+
+      select.addEventListener("click", () => {
+          select.classList.toggle("select-clicked");
+
+          caret.classList.toggle("caret-rotate");
+
+          menu.classList.toggle("menu-open");
+      });
+
+      options.forEach(option => {
+          option.addEventListener("click", () => {
+              selected.innerText = option.innerText;
+              select.classList.remove("selected-clicked");
+              caret.classList.remove("caret-rotate");
+              menu.classList.remove("menu-open");
+      
+              options.forEach(option => {
+                  option.classList.remove("active");
+              });
+      
+              option.classList.add("active");
+          }); 
+      });
+    });
+  });
+
   /**
    * This function is used to clear all of the items we need when the parcours is changing.
    * @params Nothing
