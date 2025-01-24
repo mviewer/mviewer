18915fa81d0008cc9d45db3d683c703f8d552323
gaetanbrl <gaetan.bruel@jdev.fr>
2025-01-24 12:03:06 +0100

    localStorage support control, allow parent div clic

--- js/cookies/localStorage.js
+++ js/cookies/localStorage.js
@@ -9,20 +9,43 @@
  */
 
 /**
- * Allows the help modal to be shown only once
+ * Utility func to control if LocalStorage is supported.
+ * @returns boolean
+ */
+export const isLocalStorageSupported = () => {
+  try {
+    // control if supported
+    return typeof Storage !== "undefined" && window.localStorage;
+  } catch (e) {
+    // not supported
+    return false;
+  }
+};
+
+/**
+ * Void function.
+ * Allows the help modal to be shown only once.
  */
 export const ncShowHelpOnce = () => {
   const helpCheckBox = document.getElementById("showHelpOnce");
-  if (!helpCheckBox) return;
-  // init
+  const helpParentBloc = document.getElementById("showHelpOnceBloc");
+  if (!helpCheckBox && !isLocalStorageSupported()) return;
+  // init and manage browser issue
   try {
     const savedState = localStorage.getItem("helpCheckBox");
     helpCheckBox.checked = savedState === "true";
-    // init on change
+    // change lcoalStorage value on checkbox change
     helpCheckBox.addEventListener("change", function () {
       localStorage.setItem("helpCheckBox", helpCheckBox.checked);
     });
+    // dispatch checkbox change event on parent to ease check action
+    helpParentBloc.addEventListener("click", () => {
+      const showNextTime = localStorage.getItem("helpCheckBox") == "true";
+      helpCheckBox.checked = !showNextTime;
+      const change = new Event("change");
+      helpCheckBox.dispatchEvent(change);
+    });
   } catch (e) {
-    mviewer.alert("Problème avec le localStorage", "alert-info");
+    console.log("LocalStorage : Error", "alert-info");
   }
 };
