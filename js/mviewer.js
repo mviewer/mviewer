a804bc1019b07f741cd84d6b60e8158a61658799
Agath21 <agathe.adam@jdev.fr>
2025-05-20 14:55:06 +0200

    Update mobile version

--- js/mviewer.js
+++ js/mviewer.js
@@ -1888,7 +1888,7 @@ mviewer = (function () {
         langitems.push(
           '<li style="padding-right:' +
             p +
-            '" type="button" class="btn mv-translate""><a href="#" idlang="' +
+            '" type="button" class="mv-translate""><a class="dropdown-item" href="#" idlang="' +
             language +
             '"><span style="margin-right: 5px;" class="flag-icon flag-icon-squared flag-icon-' +
             icon +
