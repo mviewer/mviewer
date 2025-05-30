a0a914f5130ddc90dcfde08543229762448a8057
Agath21 <agathe.adam@jdev.fr>
2025-05-30 16:34:23 +0200

    Update themes with var and delete font, scss, build

--- css/build_themes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for theme in 'alizarin' 'amethyst' 'black' 'blue' 'carrot' 'chambray' 'default' 'green' 'green_sea' 'nephritis' 'peter_river' 'pink' 'ripe_lemon' 'wet_asphalt'
-do
-        sass scss/$theme.scss themes/$theme.css;
-done
