1a4104114c40e31da6dbb774d980bbf4e9baef03
Agath21 <agathe.adam@jdev.fr>
2025-07-02 12:35:28 +0200

    Add toast component like alerts

--- js/mviewer.js
+++ js/mviewer.js
@@ -409,6 +409,34 @@ mviewer = (function () {
     }, duration);
   };
 
+  /**
+   * _messageToast Show toast method.
+   * @param {String} title
+   * @param {String} msg
+   */
+
+  var _messageToast = function (title,msg,delay) {
+    const toast = document.createElement('div');
+      toast.className = 'toast';
+      toast.setAttribute('role', 'alert');
+      toast.setAttribute('aria-live', 'assertive');
+      toast.setAttribute('aria-atomic', 'true');
+      toast.setAttribute('data-bs-delay', delay ?? 5000);
+      toast.innerHTML = `
+        <div class="toast-header">
+          <strong class="me-auto">${title}</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Fermer"></button>
+        </div>
+        <div class="toast-body">
+          ${msg}
+        </div>
+      `;
+      document.getElementById('toasts-zone').appendChild(toast);
+      const bsToast = new bootstrap.Toast(toast);
+      bsToast.show();
+      toast.addEventListener('hidden.bs.toast', () => toast.remove());
+  };
+
   var _deleteLayer = function (layername) {
     $("[data-layerid='" + layername + "']").remove();
     _map.removeLayer(_overLayers[layername].layer);
@@ -3743,6 +3771,10 @@ mviewer = (function () {
 
     alert: function (msg, cls, ms) {
       _message(msg, cls), ms;
+    },
+
+    toast: function (title, msg) {
+      _messageToast(title, msg);
     },
 
     legendSize: function (img) {
