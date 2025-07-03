e2f86ce46e647b712cfdfc027851afcd28e8b728
Agath21 <agathe.adam@jdev.fr>
2025-07-03 16:17:33 +0200

    Add opengraph #1094

--- js/configuration.js
+++ js/configuration.js
@@ -356,7 +356,8 @@ var configuration = (function () {
     }
     if (conf.application.title || API.title) {
       var title = API.title || conf.application.title;
-      document.title = title;
+      document.title = title;      
+      document.querySelector('meta[property="og:title"]').setAttribute("content", title);
       title = conf.application.htmltitle || title;
       $(".mv-title").text("");
       $(".mv-title").append(title);
